671bba5ad62eae0ad13d4fa37457e70d33b12c98
tfqqrman <tfqqrman@gmail.com>
2022-03-06 22:09:39 +0700

    Finishing RecyclerView listMode

--- app/src/main/java/com/example/warfoodsubmission/MainActivity.kt
+++ app/src/main/java/com/example/warfoodsubmission/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.warfoodsubmission
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvFood: RecyclerView
+    private var list:ArrayList<Food> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvFood = findViewById(R.id.rv_food)
+        rvFood.setHasFixedSize(true)
+
+        list.addAll(FoodData.listData)
+        showRecyclerView()
+    }
+    private fun showRecyclerView(){
+        rvFood.layoutManager = LinearLayoutManager(this)
+        val listFoodAdapter = ListFoodAdapter(list)
+        rvFood.adapter = listFoodAdapter
     }
 }
